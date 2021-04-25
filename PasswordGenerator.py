7beb34c500b4b3535b0995f3130214cbed987a14
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-04-24 23:11:56 -0400

    Hangman Game start

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,8 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
+'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -7,19 +10,6 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-# password = ""
-
-# for char in range(1, nr_letters + 1):
-#     password += random.choice(letters)
-
-# for char in range(1, nr_symbols + 1):
-#     password += random.choice(symbols)
-
-# for char in range(1, nr_numbers + 1):
-#     password += random.choice(numbers)
-
-# print(password)
 
 password_list = []
 
@@ -40,3 +30,21 @@ for char in password_list:
     password += char
 
 print(f"Your password is: {password}")
+
+
+
+
+
+
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
+
+# print(password)
