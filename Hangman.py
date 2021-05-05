714f0f4eb7e1113167ccf995023072ded39d2a3e
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-05-04 22:09:01 -0400

    Hangman complete

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,44 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+import Hangman_words
 
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(Hangman_words.word_list)
 
-guess = (input("Guess a letter: ")).lower()
+lives = 6
+
+from hangman_art import logo, stages
+print(logo)
+
+display = []
 
 for letter in chosen_word:
-    if letter == guess:
-        print("true")
-    else:
-        print("false")
+    display += "_"
+print (display)
+
+end_of_game = False
+
+while not end_of_game:
+    guess = (input("Guess a letter: ")).lower()
+
+    if guess in display:
+        print(f"You already guesses {guess}")
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print (display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
